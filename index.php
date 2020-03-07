e79f9ab766f45abefa4c6057788a01aa0879f4dd
pabloperez2020 <60245385+pabloperez2020@users.noreply.github.com>
2020-03-06 20:45:56 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" href="css/style.css">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PRACTICA 10</title>
+</head>
+<body>
+<?php
+//comentario 1 linea 
+/*comentario
+multilane*/
+echo"<h1>hola mundo PHP en construccion</h1>";
+?>  
+</body>
+</html>
+
+
+
